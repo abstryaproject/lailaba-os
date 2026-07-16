3f155f11b3c9e5cf59c887d2db9941bc04702a9b
A. I. LAILABA <abstryaproject@users.noreply.github.com>
2026-07-16 10:16:08 +0100

    fix: drop explicit security-mirror flags (live-build derives them from distribution)

--- build.sh
+++ build.sh
@@ -43,9 +43,7 @@ lb config \
   --iso-publisher "abstryaproject" \
   --iso-volume "LailabaOS" \
   --mirror-bootstrap "http://deb.debian.org/debian" \
-  --mirror-debian-security "http://security.debian.org/debian-security" \
   --mirror-binary "http://deb.debian.org/debian" \
-  --mirror-binary-security "http://security.debian.org/debian-security" \
   --archive-areas "main contrib non-free non-free-firmware"
 
 # 3) Package list (what the ISO ships)
